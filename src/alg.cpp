a47f8c0b69079f0c5308a39b458dd37d3ed17576
Kirill Kashkirov <124998794+Kashkirov@users.noreply.github.com>
2023-02-18 17:39:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,8 @@
 int gcd(int a, int b) {
     int div = 0;
     int i = 1;
-    while (a > i || b > i){
-        if ((0 == a % i) && (0 == b % i)){
+    while (a > i || b > i) {
+        if ((0 == a % i) && (0 == b % i)) {
             div = i;
         }
         i++;

ed5850b0d6ebeb140effc8b86dd65ecc05649af7
Kirill Kashkirov <124998794+Kashkirov@users.noreply.github.com>
2023-02-18 16:49:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,15 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    int div = 0;
+    int i = 1;
+    while (a > i || b > i)
+    {
+        if ((0 == a % i) && (0 == b % i))
+        {
+            div = i;
+        }
+        i++;
+    }
+    return div;
 }
